a53a05e142292e41c5efdaf25b6fc7992ce55751
Nohpanaht1001x2 <thanaphon_bo@kkumail.com>
2023-05-22 14:54:53 +0700

    make Http server render html

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -8,10 +8,16 @@ public class Client {
 //        Socket socket = new Socket("192.168.31.55",8080);
         Socket socket = new Socket("192.168.31.55",8080);
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        BufferedReader client = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferWritter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        bufferWritter.write("momomm op");
-//        bufferWritter.write(bufferedReader.readLine());
+
+//        bufferWritter.write("momomm op");
+//        bufferWritter.write(client.readLine());
+
+        bufferWritter.write("GET http://localhost:8080");
+        bufferWritter.newLine();
         bufferWritter.flush();
+        System.out.println(bufferedReader.readLine());
         socket.close();
 
     }
--- src/main/java/org/example/server/HttpServer.java
+++ src/main/java/org/example/server/HttpServer.java
@@ -1,6 +1,7 @@
 package org.example.server;
 
 import java.io.*;
+import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalDate;
@@ -8,19 +9,48 @@ import java.time.LocalDate;
 public class HttpServer {
     public void start() throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080);
-        Socket acceptSocket = serverSocket.accept();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(acceptSocket.getInputStream()));
-        BufferedWriter bufferWritter = new BufferedWriter(new OutputStreamWriter(acceptSocket.getOutputStream()));
-        //buffWritter use to respound the client
-        //by write string to their output
-        String input = bufferedReader.readLine();
-//        String string = bufferedReader.readLine();
-        System.out.println("method : "+input.split(" ")[0]
-                +"\n"+"method : "+input.split(" ")[1]);
-//        bufferWritter.write(input +" "+string+" "+ LocalDate.now());
-        bufferWritter.newLine();    //to shift close connection message to new line
-        bufferWritter.flush();
-        System.out.println(input);
-        serverSocket.close();
+        while (true) {
+            Socket acceptSocket = serverSocket.accept();
+            new Thread(
+                    () -> {
+                        try {
+                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(acceptSocket.getInputStream()));
+                            BufferedWriter bufferWritter = new BufferedWriter(new OutputStreamWriter(acceptSocket.getOutputStream()));
+                            //buffWritter use to respond the client
+                            //by write string to their output
+                            String input = bufferedReader.readLine();
+                            //        String string = bufferedReader.readLine();
+                            System.out.println("method : " + input.split(" ")[0]
+                                    + "\n" + "Resource : " + input.split(" ")[1]);
+                            //        bufferWritter.write(input +" "+string+" "+ LocalDate.now());
+//                            bufferWritter.newLine();    //to shift close connection message to new line
+
+                            bufferWritter.write("HTTP/1.1 200");
+                            bufferWritter.newLine();    //http response newline
+                            bufferWritter.newLine();    //http response newline
+                            // these 2 newline is require to make html payload to render
+                            //because it standard format to transfer webpage
+                            //i guess lol
+                            bufferWritter.write(
+                                    "<html>" +
+                                            "<body>" +
+                                            "<h1>" +
+                                            "<p align=\"center\">"+
+                                            "hello world" +
+                                            "</p>"+
+                                            "</h1>" +
+                                            "</body>" +
+                                            "</html>"
+                                            );
+
+
+                            bufferWritter.flush();
+                            System.out.println(input);
+//                            serverSocket.close();
+                        } catch (Exception e) {
+                            throw new RuntimeException();
+                        }
+                    }).start();
+        }
     }
 }
